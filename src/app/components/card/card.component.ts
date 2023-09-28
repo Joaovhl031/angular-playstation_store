430beef412780754daee824d53c62c55fb5c546b
Joao <joaohenrique9021@gmail.com>
2023-09-28 18:03:58 -0300

    update config files

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+	@Input()
+	gameCover:string =''
+	@Input()
+	gameLabel:string= 'Exclusive'
+	@Input()
+	gameType:string = 'Digital PS4'
+	@Input()
+	gamePrice:string = 'R$ 349,98'
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
